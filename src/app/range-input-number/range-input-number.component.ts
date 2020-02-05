398bad7d8e0907b46817a268b6549992ba195d59
Kento Hayase <kento.hayase@basic.co.jp>
2020-02-05 19:57:30 +0900

    移植

--- src/app/range-input-number/range-input-number.component.ts
+++ src/app/range-input-number/range-input-number.component.ts
@@ -1,32 +1,27 @@
-import { Component, OnInit, Input } from "@angular/core";
-import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { Component, OnInit, Input } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-    selector: "app-range-input-number",
-    templateUrl: "./range-input-number.component.html",
-    styleUrls: ["./range-input-number.component.scss"]
+    selector: 'app-range-input-number',
+    templateUrl: './range-input-number.component.html',
+    styleUrls: ['./range-input-number.component.scss'],
 })
 export class RangeInputNumberComponent implements OnInit {
     fromNum: number;
     toNum: number;
     msg: String;
+    pattern: string | RegExp;
     isWrongOrder: boolean = true;
-    @Input() public pattern: string | RegExp;
+    @Input() minLength: number;
+    @Input() maxLength: number;
 
     constructor() {}
 
     ngOnInit() {
-        if (this.pattern === "[0-9]+$") {
-            this.msg = "半角数字のみ入力してください。";
-        }
+        this.pattern = '[0-9]+$';
     }
 
     compareOrder() {
-        //     if (Number(this.fromNum) > Number(this.toNum)) {
-        //         this.isWrongOrder = false;
-        //     } else {
-        //         this.isWrongOrder = true;
-        // 	}
         this.isWrongOrder = Number(this.fromNum) < Number(this.toNum);
     }
 }
